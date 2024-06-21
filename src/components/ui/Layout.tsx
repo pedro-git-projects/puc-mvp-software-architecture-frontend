47759d6c63c4fb4d4fa8e6f4d6b1a535012db88d
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-06-21 18:10:51 -0300

    feat: footer

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header isAuthenticated={true} />
       <main className="flex-grow min-h-screen">{children}</main>
+      <Footer />
     </>
   );
 }
